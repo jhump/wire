0020fbbf0f54896ff3851b9f8d3cadf4be0594b4
Alan Chiu <awlchiu@gmail.com>
2022-09-02 11:13:18 -0700

    add java

--- wire-library/wire-protoc/src/main/java/com/squareup/wire/protocwire/cmd/Generators.kt
+++ wire-library/wire-protoc/src/main/java/com/squareup/wire/protocwire/cmd/Generators.kt
@@ -2,8 +2,20 @@ package com.squareup.wire.protocwire.cmd
 
 import com.squareup.wire.protocwire.Plugin
 import com.squareup.wire.protocwire.WireGenerator
+import com.squareup.wire.schema.JavaTarget
 import com.squareup.wire.schema.KotlinTarget
 import com.squareup.wire.schema.SwiftTarget
+
+class JavaGenerator {
+
+  companion object {
+    @JvmStatic
+    fun main(args: Array<String>) {
+      val target = JavaTarget(outDirectory = "")
+      Plugin.run(WireGenerator(target))
+    }
+  }
+}
 
 class KotlinGenerator {
 
